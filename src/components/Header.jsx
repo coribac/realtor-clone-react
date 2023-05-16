8bb7b4b7dd758296fc54f21670e6d63fe6cabce0
hocine <hocinecoribac@gmail>
2023-05-16 10:52:37 +0100

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 import Logo from '../images/rdc-logo-default.svg'
 import { useLocation, useNavigate } from 'react-router-dom';
-
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 export default function Header() {
+  const [pageState,setPageState]=useState("sign-in")
+  const auth =getAuth()
     const location=useLocation()
     const navigate = useNavigate()
+    useEffect(()=>{
+      onAuthStateChanged(auth,(user)=>{
+        if(user){
+          setPageState("profile")
+        }else{
+          setPageState("sign-in")
+        }
+      })
+    },[auth])
+
     function pathMatchRoute(route){
       if(route===location.pathname){
         return true
@@ -43,11 +55,13 @@ export default function Header() {
                 >
                 Offers</li>
                 <li
-                className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+                className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                  (pathMatchRoute("/sign-in")|| pathMatchRoute("/sign-in")) && "text-black border-b-red-500"
+                }
                   `}
-                  onClick={() => navigate("/sign-in")}
+                  onClick={() => navigate("/profile")}
                 >
-                Sign In
+                {pageState}
                 </li>
                </ul>
              </div>
